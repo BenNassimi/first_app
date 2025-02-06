8c7e8daff698279a308b06450edcb41fb66ef5ae
Ben Nassimi <BenNassimi456@gmail.com>
2025-02-06 11:40:48 -0500

    Finished gradent container and centered

--- lib/gradent_container.dart
+++ lib/gradent_container.dart
@@ -1,30 +1,24 @@
+import 'package:first_app/centered.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget{
+  const GradientContainer({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 9, 1, 24),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 34,
-              )
-            ),
-          ),
-        );
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 9, 1, 24),
+          ],
+        ),
+      ),
+      child: Centered("Welcome ITEC 315")
+    );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:first_app/gradent_container.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer()
+        body: GradientContainer(),
       ),
     ),
   );
