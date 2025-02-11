bc47c5e7d2d474e333c6b939a3caa6d6af9407a8
Ben Nassimi <BenNassimi456@gmail.com>
2025-02-11 12:33:42 -0500

    Finished dice project

--- lib/gradent_container.dart
+++ lib/gradent_container.dart
@@ -1,13 +1,19 @@
 import 'package:first_app/centered.dart';
-import 'package:flutter/widgets.dart';
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatelessWidget{
-  GradientContainer(this.startAlignment, this.endAlignment,{super.key});
+class GradientContainer extends StatelessWidget {
+  GradientContainer(this.startAlignment, this.endAlignment, {super.key});
   Alignment startAlignment;
   Alignment endAlignment;
   @override
   Widget build(BuildContext context) {
+    var activeDiceImage = 'assets/images/dice-2.png';
+    void rollDice() {
+      activeDiceImage = 'assets/images/dice-4.png';
+      print("Rolling the dice");
+    }
+
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -19,11 +25,8 @@ class GradientContainer extends StatelessWidget{
           ],
         ),
       ),
-      child: Center(
-        child: Image.asset(
-          'assets/images/dice-2.png',
-          width:200,
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
